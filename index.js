fe58ec3c99e0501217925d0c89356839205c435b
Bibhu Prasad Dash <bibhuprasad.dash@estpl.in>
2023-09-04 17:50:39 +0530

    cors handle

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dbUrl=require('./src/config/db');
 const userRoute=require('./src/routes/route')
+const cors = require('cors');
 const app=express();
 
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use('/auth',userRoute)
+app.use(cors());
 
 app.get('/',(req,res)=>{
     return res.send({message:"Hello world from homepage"})
